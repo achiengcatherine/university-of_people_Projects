0f41e7ae3bee05cd5858a5d43a9a7cb205d56fff
achiengcatherine <achiengcatherine431@gmail.com>
2022-01-02 20:47:12 +0300

    shows a Message dialog stating '<power> TO THE RESCUE, where'<power> is the input converted to upper case?

---
+++ UoP-Java_Projects/super_power.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class Superpower {
+	public static void main (String[] args) {
+		String power = JOptionPane.showInputDialog("What is your superpower?");
+		power = power.toUpperCase();
+		JOptionPane.showMessageDialog(null,power+ " " +"TO THE RESCUE!");
+	}
+
+
+}
